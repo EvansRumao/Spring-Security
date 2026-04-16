5e3fc07572f88b74f300f213b7295a7ef33c85a8
Evans <Evansrumao30@gmail.com>
2026-04-16 20:10:50 +0530

    config work

--- src/main/java/com/Security/SpringSecurity/controller/Controller.java
+++ src/main/java/com/Security/SpringSecurity/controller/Controller.java
@@ -1,5 +1,7 @@
 package com.Security.SpringSecurity.controller;
 
+
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
 
     @GetMapping("hello")
-    public String greed(){
-        return "hello";
+    public String greed(HttpServletRequest request){
+        return "hello"+request.getSession().getId();
     }
 }
